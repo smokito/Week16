0f1b58706072b882ff529503c9a70c2875bd9449
smokito <anna.aksiuk.aa@gmail.com>
2022-08-28 21:01:12 +0200

    added changes in JS

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 function sumInput() {
     let input = document.querySelector('#num');
     let nums = [];
-    let enter = document.querySelector('#ok');
-    enter.addEventListener("click", enterNums)
-    function enterNums() {
+    let okBtn = document.querySelector('#ok');
+    let readyBtn = document.querySelector('#ready');
+    okBtn.addEventListener("click", enterNums);
+    function enterNums()
+    {
+let value = input.value;
+if (!isNaN(value)) {
     alert ('введите число')
-    let value = input.value;
-    input.value = ''
-    while (true) {
-        if (value === null)
-        break;
-        nums.push(+value);
-
-    }
+    input.value = '';
+    nums.push(+value);
 }
+else {
+alert ('Введеное значение не является числом');
+showResult(nums)
+}
+}
+}
+function showResult(arr) {
+arr.sort();
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+alert('filtered: ' +  arr + ' sum: ' + sum);
 }
 sumInput()
